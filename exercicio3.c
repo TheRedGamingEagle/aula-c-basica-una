3d4d4bad58ebcf493c5dd190e51a38dfdff97e3c
rmmariano <54472840+TheRedGamingEagle@users.noreply.github.com>
2024-08-28 18:07:24 -0300

    Meti o loko e resolvi com complexidade O(√N)

--- exercicio3.c
+++ exercicio3.c
@@ -1,22 +1,53 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 
+bool primo(int N)
+{
+    //Complexidade de tempo O(√N)
+    //Complexidade de espaço O(1)
+    // Essa parte é obvia
+    if (N <= 1) {
+        return false;
+    }
+
+    // Essa tbm (2 e 3 é primo)
+    if (N <= 3) {
+        return true;
+    }
+
+    // Isso aq ja tira uma cacetada de numero
+    if (N % 2 == 0 || N % 3 == 0) {
+        return false;
+    }
+
+    // Procura Divisores de 5 ate raiz de N
+    for (int i = 5; i * i <= N; i += 6) {
+      
+        // Se N é divisivel por i ou (i + 2), não é primo
+        
+        if (N % i == 0 || N % (i + 2) == 0) {
+            return false;
+        }
+    }
+
+    // Se n tiver divisores é primo
+    return true;
+}
+
+// Codigo Principal
 int main()
 {
+    int num;
+    printf("Digite um número: ");
+    scanf("%d", &num);
 
-    int nota = 85;
+    if (primo(num)) {
+        printf("Seu Numero É Primo");
+    }
+    else {
+        printf("Seu Numero Não É Primo");
+    }
 
-    if (nota >= 90)
-    {
-        printf("Nota A\n");
-    }
-    else if (nota >= 80)
-    {
-        printf("Nota B\n");
-    }
-    else
-    {
-        printf("Nota C\n");
-    }
-    
     return 0;
 }
