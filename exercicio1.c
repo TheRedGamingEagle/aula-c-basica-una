cb9f7e0b2d8eb118b2f89dc882919d316d1daeb8
rmmariano <54472840+TheRedGamingEagle@users.noreply.github.com>
2024-08-28 17:42:36 -0300

    exercicio1

--- exercicio1.c
+++ exercicio1.c
@@ -2,6 +2,23 @@
 
 int main()
 {
-    printf("Olá, Mundo!\n");
+    int num;
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) 
+    {
+
+        printf("Seu Numero é Par")
+
+    }
+
+    else
+    {
+
+        printf("Seu Numero É Impar")
+
+    }
+
     return 0; // Retorna 0 para indicar que o programa terminou corretamente
 }
