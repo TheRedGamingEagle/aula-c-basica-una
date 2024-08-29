098a7bb27bd1bc0f702f9036b5247f148f761582
rmmariano <54472840+TheRedGamingEagle@users.noreply.github.com>
2024-08-29 00:20:56 -0300

    Say, can you see By the dawn's early light What so proudly we hailed At the twilight's last gleaming? Whose broad stripes and bright stars Through the perilous fight O'er the ramparts we watched Were so gallantly, yeah, streaming? And the rockets' red glare The bombs bursting in air Gave proof through the night That our flag was still there O say, does that star-spangled banner yet wave O'er the land of the free and the home of the brave

--- exercicio9.c
+++ exercicio9.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-void saudacao() {
-    printf("Bem-vindo ao programa!\n");
-}
-
 int main() {
-    saudacao();
+    float celsius, fahrenheit;
+
+    printf("Digite a temperatura em graus Celsius (graus de gente normal): ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("A temperatura em Fahrenheit (graus de americano burro) é: %.2f°F\n", fahrenheit);
 
     return 0;
 }
