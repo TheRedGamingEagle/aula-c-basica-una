323b706ca569face1efb50afe26bbe2a013cb714
rmmariano <54472840+TheRedGamingEagle@users.noreply.github.com>
2024-08-28 23:56:23 -0300

    Ingredientes: Para o bolo: 3 cenouras médias (picadas) 3 ovos 1 xícara de óleo (240ml) 2 xícaras de açúcar 2 e 1/2 xícaras de farinha de trigo 1 colher (sopa) de fermento em pó Para a cobertura de chocolate: 1/2 xícara de leite 4 colheres (sopa) de achocolatado ou chocolate em pó 1 colher (sopa) de manteiga ou margarina 3 colheres (sopa) de açúcar Modo de preparo: Para o bolo: Pré-aqueça o forno a 180°C. No liquidificador, bata as cenouras picadas, os ovos e o óleo até obter uma mistura homogênea. Em uma tigela grande, peneire a farinha de trigo e o açúcar. Despeje a mistura de cenoura sobre os ingredientes secos e mexa bem até formar uma massa lisa. Adicione o fermento e misture delicadamente. Despeje a massa em uma forma untada e enfarinhada. Asse no forno pré-aquecido por aproximadamente 40 a 50 minutos, ou até que um palito inserido no centro saia limpo. Para a cobertura de chocolate: Em uma panela, misture o leite, o achocolatado (ou chocolate em pó), a manteiga e o açúcar. Cozinhe em fogo médio, mexendo sempre, até a calda engrossar um pouco (cerca de 5 minutos). Despeje a cobertura sobre o bolo ainda quente. Dicas: Se quiser uma cobertura mais grossa, deixe a calda no fogo por mais tempo. Decore o bolo com granulado ou raspas de chocolate para um toque especial. Esse bolo de cenoura é perfeito para um café da tarde ou como sobremesa. Bom apetite!

--- exercicio5.c
+++ exercicio5.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
+    char str[10000], temp;
+    int i, j;
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Iteração %d\n", i);
+    // pede a str pro user
+    printf("Digite uma string ai man: ");
+    fgets(str, sizeof(str), stdin);
+
+    // remove o caractere de nova linha (tava bugando pkrl quando tinha lb)
+    str[strcspn(str, "\n")] = '\0';
+
+    // pega o tamanho (lá ele kskssksksk)
+    int length = strlen(str);
+
+    // troca os caractere do inicio pelso do final
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
     }
+
+    // Exibe a string invertida
+    printf("String invertida: %s\n", str);
 
     return 0;
 }
