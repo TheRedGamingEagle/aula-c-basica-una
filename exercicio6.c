34368000b370edada94c33304465b60c63f4bbd9
rmmariano <54472840+TheRedGamingEagle@users.noreply.github.com>
2024-08-28 23:59:17 -0300

    Complexidade Temporal alta pkrl mas é o que tem pra hj

--- exercicio6.c
+++ exercicio6.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-    int i = 0;
-    while (i < 5)
-    {
-        printf("i = %d\n", i);
-        i++;
+int fatorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * fatorial(n - 1);
     }
+}
+
+int main() {
+    int num;
+    printf("Fala o numero ai q eu te falo o fatorial: ");
+    scanf("%d", &num);
+
+    printf("%d! = %d\n", num, fatorial(num));
 
     return 0;
 }
